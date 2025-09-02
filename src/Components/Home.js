02e97d3fe889c863f26873ffd6756857cf7e5819
Harshit Srivastava <Harshitsrivastava0914@gmail.com>
2025-09-02 12:04:43 +0530

    C2.1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,10 +6,10 @@ function Home() {
       <div>
       <img src={P2} alt="Greenliness Wall" className="Homewall1"/>
       </div>
-    <div className="Home">
-      <h1 className="title">Welcome to Greenliness</h1>
-      <p className="subtitle">Your one-stop solution for eco-friendly products and tips.</p>
-    </div>
+      <div className="Home">
+        <h1 className="title">Welcome to Greenliness</h1>
+        <p className="subtitle">Your one-stop solution for eco-friendly products and tips.</p>
+      </div>
     </div>
     </>
   );
