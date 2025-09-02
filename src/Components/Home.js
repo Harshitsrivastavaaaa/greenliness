760f546376c49a0c6fcd87c2ea5b507040128fab
Harshit Srivastava <Harshitsrivastava0914@gmail.com>
2025-09-02 12:00:54 +0530

    C2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,17 @@
+import P2 from '../Assets/P2.jpg';
 function Home() {
   return (
+    <>
+    <div className="Homewall">
+      <div>
+      <img src={P2} alt="Greenliness Wall" className="Homewall1"/>
+      </div>
     <div className="Home">
-      <h1>Welcome to Greenliness</h1>
-      <p>Your one-stop solution for eco-friendly products and tips.</p>
+      <h1 className="title">Welcome to Greenliness</h1>
+      <p className="subtitle">Your one-stop solution for eco-friendly products and tips.</p>
     </div>
+    </div>
+    </>
   );
 }
 
